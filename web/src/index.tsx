419f811cc6e35062ff13d1e9b1860b89f19b24f6
Jean Junior <jclgjunior@gmail.com>
2020-10-12 23:18:01 -0300

    Change router

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { BrowserRouter as Router } from 'react-router-dom'
 
-import GlobalStyle from './styles/global'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
       <App />
-      <GlobalStyle />
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
