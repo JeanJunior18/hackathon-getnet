37f9bb890e32febe8c915d590127cb6b9f933df5
Jean Junior <jclgjunior@gmail.com>
2020-10-12 23:09:18 -0300

    Updates

---
+++ precache-manifest.f16634d262f755b4dc62ff7bc8599a86.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acf1390b88f64956efeda211c40bf6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c252b876ba2e261ca1cb",
+    "url": "/static/js/2.b6219c75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b6219c75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c5e22a4d04772bdcf8",
+    "url": "/static/js/main.7ec78ad9.chunk.js"
+  },
+  {
+    "revision": "2e0e95700a5ba086426d",
+    "url": "/static/js/runtime-main.9beb02a6.js"
+  },
+  {
+    "revision": "01ae591d79741097043e1d224feb17b3",
+    "url": "/static/media/chart.01ae591d.png"
+  },
+  {
+    "revision": "45b8c53a5edde01368d258507dff018d",
+    "url": "/static/media/configuracao.45b8c53a.svg"
+  },
+  {
+    "revision": "9136b0c92d84519ba46d7be35b1b88e6",
+    "url": "/static/media/consultoria.9136b0c9.svg"
+  },
+  {
+    "revision": "85a3afbaedb6b650ea937d4ea335564b",
+    "url": "/static/media/estoque.85a3afba.svg"
+  },
+  {
+    "revision": "77a35c99571fff4b82d30b59e9b1e0e3",
+    "url": "/static/media/financeiro.77a35c99.svg"
+  },
+  {
+    "revision": "a16568b2ef1d988ad824e6f9ddad22da",
+    "url": "/static/media/fornecedores.a16568b2.svg"
+  },
+  {
+    "revision": "9f05fa80d79463334cba7e9e9dbf3875",
+    "url": "/static/media/getnet-logo.9f05fa80.svg"
+  },
+  {
+    "revision": "7aa466d92b81900dd9c9820256f4f204",
+    "url": "/static/media/investimento.7aa466d9.svg"
+  },
+  {
+    "revision": "9a31ccfe4a8bbcd460dd1640ecafb85c",
+    "url": "/static/media/pizza.9a31ccfe.svg"
+  },
+  {
+    "revision": "511374b8cf5d535787d36f100d9b7c15",
+    "url": "/static/media/rec1.511374b8.png"
+  },
+  {
+    "revision": "3ad1c2d880569f3e691db9c0bf6d665d",
+    "url": "/static/media/rec2.3ad1c2d8.png"
+  },
+  {
+    "revision": "388a0051fb81683562bdb8588eb97668",
+    "url": "/static/media/rec3.388a0051.png"
+  },
+  {
+    "revision": "b6137034313a8ed571b5defdc1907e76",
+    "url": "/static/media/rec4.b6137034.png"
+  },
+  {
+    "revision": "9f6d2c8925b224ebdb72d9e149fa08e4",
+    "url": "/static/media/relatorio.9f6d2c89.svg"
+  },
+  {
+    "revision": "3148e0760bd9097cc6a992545d7d8005",
+    "url": "/static/media/servicos.3148e076.svg"
+  },
+  {
+    "revision": "eb3ceb01570052ca36a4f4448693eea6",
+    "url": "/static/media/trilha.eb3ceb01.svg"
+  }
+]);
